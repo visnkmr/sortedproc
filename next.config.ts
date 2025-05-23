798d9fb777f41db3f90d4566d85de387a78d2fd7
12core1 <test@someone.local>
2025-05-23 22:17:19 +0530

    move ci to bun

--- next.config.ts
+++ next.config.ts
@@ -24,3 +24,13 @@ export default withPWA({
   // disable: process.env.NODE_ENV === 'development',
 })({});
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: process.env.REPOSITORY_NAME,
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
